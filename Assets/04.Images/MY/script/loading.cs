368c1ed1def34730e9e7b036e731c56fac1d1377
bin549 <549288160@qq.com>
2021-06-23 10:50:04 +0800

    Assets

---
+++ Assets/04.Images/MY/script/loading.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loading : MonoBehaviour
+{
+	//进度条 image
+    public Image m_Image;
+    //显示的进度文字 100%
+    public Text m_Text;
+    //控制进度
+    float m_CurProgressValue = 0;
+    float m_ProgressValue = 100;
+
+    void Update()
+    {
+        if (m_CurProgressValue < m_ProgressValue)
+        {
+            m_CurProgressValue++;
+        }
+        //实时更新进度百分比的文本显示 
+        m_Text.text = m_CurProgressValue + "%";
+        //实时更新滑动进度图片的fillAmount值  
+        m_Image.GetComponent<Image>().fillAmount = m_CurProgressValue / 100f;
+        if (m_CurProgressValue == 100)
+        {
+            m_Text.text = "OK";
+            //这一块可以写上场景加载的脚本
+        }
+    }
+}
---
+++ Assets/04.Images/UI/MY/cooperation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cooperation : Button
+{
+    enum Selection
+    {
+        Normal,
+        Highlighted,
+        Pressed,
+        Disabled
+    }
+    Selection selection;
+
+    protected override void DoStateTransition(SelectionState state, bool instant)
+    {
+        base.DoStateTransition(state, instant);
+        switch (state)
+        {
+        	//四种状态
+            case SelectionState.Normal:
+                selection = Selection.Normal;
+                break;
+            case SelectionState.Highlighted:
+                selection = Selection.Highlighted;
+                break;
+            case SelectionState.Pressed:
+                selection = Selection.Pressed;
+                break;
+            case SelectionState.Disabled:
+                selection = Selection.Disabled;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin.box.fontSize = 10;
+        switch (selection)
+        {
+            case Selection.Highlighted:
+                GUI.Box(new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, 100, 25), "Cooperation");
+                break;
+            case Selection.Pressed:
+                GUI.Box(new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, 100, 25), "Pressed");
+                break;
+            default:
+                break;
+        }
+    }
+}
